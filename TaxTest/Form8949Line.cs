2d3a354e0ffeffc39c68c7d1d23c54de0ed04f0e
Austin Wise <AustinWise@gmail.com>
2021-02-21 14:26:52 -0800

    Figure out how to check boxes.
    After a more careful reading of the form when getting ready to automate
    filling it, I came to the conclusion that I only had to enter 2 lines, not
    50. So I'm less motivated to finish writing this.

---
+++ TaxTest/Form8949Line.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace TaxTest
+{
+    enum Form8949Code
+    {
+        /// <summary>
+        /// Short-term transactions reported on Form(s) 1099-B showing basis was reported to the IRS
+        /// </summary>
+        A,
+        /// <summary>
+        /// Short-term transactions reported on Form(s) 1099-B showing basis wasn’t reported to the IRS
+        /// </summary>
+        B,
+        /// <summary>
+        ///  Short-term transactions not reported to you on Form 1099-B
+        /// </summary>
+        C,
+
+        /// <summary>
+        /// Long-term transactions reported on Form(s) 1099-B showing basis was reported to the IRS
+        /// </summary>
+        D,
+        /// <summary>
+        /// Long-term transactions reported on Form(s) 1099-B showing basis wasn’t reported to the IRS
+        /// </summary>
+        E,
+        /// <summary>
+        /// Long-term transactions not reported to you on Form 1099-B
+        /// </summary>
+        F,
+    }
+
+    class Form8949Line
+    {
+        public Form8949Line(XElement el)
+        {
+            this.Code = Enum.Parse<Form8949Code>(el.Element("FORM8949CODE").Value);
+            throw new NotImplementedException();
+        }
+
+        public Form8949Code Code { get; }
+        public DateTime? Acquired { get; }
+        public DateTime Sold { get; }
+        public string Description { get; }
+        public Money CostBasis { get; }
+        public Money SalePrice { get; }
+        public string SecName { get; }
+        public double NumberOfShares { get; }
+    }
+}
--- TaxTest/Program.cs
+++ TaxTest/Program.cs
@@ -1,5 +1,8 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.Forms.Fields;
+using iText.Kernel.Pdf;
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace TaxTest
 {
@@ -14,6 +17,61 @@ namespace TaxTest
             var form = iText.Forms.PdfAcroForm.GetAcroForm(doc, false);
             var field = form.GetField("topmostSubform[0].Page1[0].Table_Line1[0].Row1[0].f1_9[0]");
             field.SetValue("lolol");
+
+            string fieldFormat = "topmostSubform[0].Page1[0].c1_1[{0}]";
+
+            var boxes = new List<PdfButtonFormField>();
+            for (int i = 0; i < 3; i++)
+            {
+                boxes.Add((PdfButtonFormField)form.GetField(string.Format(fieldFormat, i)));
+            }
+
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                var box = boxes[i];
+                var checkState = box.GetAppearanceStates()[0];
+                box.SetFieldFlag(32768, true);
+                box.SetValue(checkState);
+                box.Put(PdfName.AS, new PdfName(checkState));
+            }
+        }
+
+        private static void PrintButtonInfo(List<PdfButtonFormField> boxes, List<Expression<Func<PdfButtonFormField, object>>> accessors)
+        {
+            foreach (var expr in accessors)
+            {
+                Console.Write("{0,20}", getMethodCallName(expr.Body));
+                var func = expr.Compile();
+                for (int i = 0; i < boxes.Count; i++)
+                {
+                    Console.Write("{0,20}", formatValue(func(boxes[i])));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static string getMethodCallName(Expression expr)
+        {
+            switch (expr)
+            {
+                case MethodCallExpression m:
+                    return m.Method.Name;
+                case UnaryExpression u:
+                    return getMethodCallName(u.Operand);
+                default:
+                    throw new NotSupportedException("Unknown type: " + expr.GetType());
+            }
+        }
+
+        static string formatValue(object val)
+        {
+            if (val is null)
+                return "<null>";
+            if (val is string[] stringArray)
+            {
+                return "[" + string.Join(", ", stringArray) + "]";
+            }
+            return val.ToString();
         }
     }
 }
