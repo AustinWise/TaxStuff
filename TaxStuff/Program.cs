232d54dd6b7d0ca5a3071c88351565f6a94c0aaa
Austin Wise <AustinWise@gmail.com>
2022-01-20 10:42:10 -0800

    Use a top-level program

--- TaxStuff/Program.cs
+++ TaxStuff/Program.cs
@@ -3,85 +3,76 @@ using System.IO;
 using TaxStuff.ExpressionEvaluation;
 using TaxStuff.FormModel;
 
-namespace TaxStuff
+if (args.Length != 2)
 {
-    class Program
+    Console.WriteLine("Requires two arguments: input.xml outputFolder");
+    return;
+}
+
+string returnPath = args[0];
+string outputFolder = args[1];
+
+if (!File.Exists(returnPath))
+{
+    throw new Exception("Tax return file does not exist: " + returnPath);
+}
+if (!Directory.Exists(outputFolder))
+{
+    throw new Exception("Output directory does not exist: " + returnPath);
+}
+
+var taxUniverse = new TaxUniverse(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+var taxReturn = new TaxReturn(returnPath, taxUniverse);
+taxReturn.Calculate();
+
+//TODO: a better way of reporting
+foreach (var formKvp in taxReturn.Forms)
+{
+    Console.WriteLine(formKvp.Key);
+    foreach (var form in formKvp.Value.Forms)
     {
-        static void Main(string[] args)
+        Console.WriteLine("\tForm");
+        var formValues = form.GetValueSnapshot();
+        foreach (var lineDef in form.Definition.Lines.Values)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Requires two arguments: input.xml outputFolder");
-                return;
-            }
+            if (!formValues.TryGetValue(lineDef.Name, out EvaluationResult value))
+                continue;
 
-            string returnPath = args[0];
-            string outputFolder = args[1];
+            Console.Write("\t\t");
+            if (lineDef.Number is not null)
+                Console.Write($"Line: {lineDef.Number} ");
+            Console.Write($"Name: {lineDef.Name}");
 
-            if (!File.Exists(returnPath))
+            if (value is ArrayResult array)
             {
-                throw new Exception("Tax return file does not exist: " + returnPath);
-            }
-            if (!Directory.Exists(outputFolder))
-            {
-                throw new Exception("Output directory does not exist: " + returnPath);
-            }
-
-            var taxUniverse = new TaxUniverse(Path.GetDirectoryName(typeof(Program).Assembly.Location));
-            var taxReturn = new TaxReturn(returnPath, taxUniverse);
-            taxReturn.Calculate();
-
-            //TODO: a better way of reporting
-            foreach (var formKvp in taxReturn.Forms)
-            {
-                Console.WriteLine(formKvp.Key);
-                foreach (var form in formKvp.Value.Forms)
+                Console.WriteLine(" Values:");
+                foreach (var v in array.Values)
                 {
-                    Console.WriteLine("\tForm");
-                    var formValues = form.GetValueSnapshot();
-                    foreach (var lineDef in form.Definition.Lines.Values)
-                    {
-                        if (!formValues.TryGetValue(lineDef.Name, out EvaluationResult value))
-                            continue;
-
-                        Console.Write("\t\t");
-                        if (lineDef.Number is not null)
-                            Console.Write($"Line: {lineDef.Number} ");
-                        Console.Write($"Name: {lineDef.Name}");
-
-                        if (value is ArrayResult array)
-                        {
-                            Console.WriteLine(" Values:");
-                            foreach (var v in array.Values)
-                            {
-                                Console.WriteLine($"\t\t\t{v}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine($" Value: {value}");
-                        }
-
-                    }
+                    Console.WriteLine($"\t\t\t{v}");
                 }
             }
-
-            foreach (var pdfForm in taxReturn.TaxYearDef.PdfInfo.Forms.Values)
+            else
             {
-                if (taxReturn.Forms.TryGetValue(pdfForm.FormName, out FormInstances formInsts))
-                {
-                    int i = 1;
-                    foreach (var inst in formInsts.Forms)
-                    {
-                        string outputFileName = formInsts.Forms.Count == 1 ? pdfForm.FormName + ".pdf" : $"{pdfForm.FormName}-{i}.pdf";
-                        string outputPath = Path.Combine(outputFolder, outputFileName);
-
-                        pdfForm.Save(outputPath, inst);
-
-                        i++;
-                    }
-                }
+                Console.WriteLine($" Value: {value}");
             }
+
+        }
+    }
+}
+
+foreach (var pdfForm in taxReturn.TaxYearDef.PdfInfo.Forms.Values)
+{
+    if (taxReturn.Forms.TryGetValue(pdfForm.FormName, out FormInstances formInsts))
+    {
+        int i = 1;
+        foreach (var inst in formInsts.Forms)
+        {
+            string outputFileName = formInsts.Forms.Count == 1 ? pdfForm.FormName + ".pdf" : $"{pdfForm.FormName}-{i}.pdf";
+            string outputPath = Path.Combine(outputFolder, outputFileName);
+
+            pdfForm.Save(outputPath, inst);
+
+            i++;
         }
     }
 }
